83f01ad7692d16ec9d25f45089197b8f8f015a6f
Alejandro Zepeda <122941448+zepedag@users.noreply.github.com>
2025-09-17 22:21:07 -0600

    Added some assets
    Some assets were added to be used for the UI.

---
+++ RickAndMortyApp/Base/ApiService.swift
@@ -0,0 +1,12 @@
+//
+//  ApiService.swift
+//  RickAndMortyApp
+//
+//  Created by Humberto Alejandro Zepeda González on 17/09/25.
+//
+
+import Foundation
+
+protocol ApíService {
+    func getDataFromGetRequest(from url: String)
+}
