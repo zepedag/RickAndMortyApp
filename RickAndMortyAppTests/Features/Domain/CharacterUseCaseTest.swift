5cdd0948db609cb857f200e5e3acb60135b73e3b
Alejandro Zepeda <122941448+zepedag@users.noreply.github.com>
2025-09-18 10:09:57 -0600

    Domain layer covered with TDD
    Created the business model of the app. The protocol 'CharacterUseCase'   is used to fetch the list of characters or to search characters by name.

---
+++ RickAndMortyAppTests/Features/Domain/CharacterUseCaseTest.swift
@@ -0,0 +1,71 @@
+//
+//  CharacterUseCaseTest.swift
+//  RickAndMortyAppTests
+//
+//  Created by Humberto Alejandro Zepeda González on 18/09/25.
+//
+
+import XCTest
+@testable import RickAndMortyApp
+
+// MARK: - Test
+class CharacterUseCaseTest: XCTestCase {
+    
+    // GIVEN
+    var sut: CharacterUseCase?
+    var sutFailure: CharacterUseCase?
+    
+    override func setUp() {
+        super.setUp()
+        sut = DefaultCharacterUseCase(repository: DefaultCharacterRepository(apiService: CharacterListFakeApiServiceSuccess()))
+        sutFailure = DefaultCharacterUseCase(repository: DefaultCharacterRepository(apiService: CharacterListFakeApiServiceFailure()))
+    }
+    
+    override func tearDown() {
+        sut = nil
+        sutFailure = nil
+        super.tearDown()
+    }
+}
+
+// MARK: - Success Tests
+extension CharacterUseCaseTest {
+    func testSuccessCase_getCharacterList() async {
+        do {
+            let response = try await sut?.getCharacterList(pageNumber: nil)
+            XCTAssertTrue(response?.results.first?.id == 21)
+        } catch {
+            XCTFail("Always receive a response and not throw an error")
+        }
+    }
+    
+    func testSuccessCase_SearchCharacter() async {
+        do {
+            let response = try await sut?.searchCharacter(by: "Rick", and: nil)
+            XCTAssertTrue(response?.results.first?.id == 21)
+        } catch {
+            XCTFail("Always receive a response and not throw an error")
+        }
+    }
+}
+
+// MARK: - Failure Tests
+extension CharacterUseCaseTest {
+    func testFailureCase_getCharacterList() async {
+        do {
+            let _ = try await sutFailure?.getCharacterList(pageNumber: nil)
+            XCTFail("This test should throw an error")
+        } catch {
+            // Test passed
+        }
+    }
+    
+    func testFailureCase_SearchCharacter() async {
+        do {
+            let _ = try await sutFailure?.searchCharacter(by: "Rick", and: nil)
+            XCTFail("This test should throw an error")
+        } catch {
+            // Test passed
+        }
+    }
+}
