1b9aaf1bbb7af64cb2e53a1c5f0fd2bd0474030f
Alejandro Zepeda <122941448+zepedag@users.noreply.github.com>
2025-09-17 23:24:47 -0600

    Cache
    Created the cache service to prioritize performance and fluidity since the app will be loading a high quantity of data including images.

--- RickAndMortyApp/Base/ApiService.swift
+++ RickAndMortyApp/Base/ApiService.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol ApíService {
+protocol ApiService {
     func getDataFromGetRequest <Response: Codable>(from url: String) async throws -> Response
     
 }
@@ -16,8 +16,18 @@ class DefaultApiService: ApiService {
     
     func getDataFromGetRequest<Response: Codable>(from url: String) async throws -> Response {
         guard let url = URL(string: url) else {
-            
+            throw AppError.invalidUrl
         }
-        
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return try JSONDecoder().decode(Response.self, from: data)
+        } catch {
+            // If decoding fails, throw a parse error
+            if (error as? DecodingError) != nil {
+                throw AppError.parseError
+            }
+            // Else throw the original service error
+            throw AppError.serviceError(error: error)
+        }
     }
 }
